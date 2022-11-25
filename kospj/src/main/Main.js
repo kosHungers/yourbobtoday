b858ddb8919e36dbe5eeffce48a17e0019f05779
Skylark92 <skylark92@live.co.kr>
2022-11-25 21:06:32 +0900

    feat : 위로 가기 버튼 구현 완료.

--- kospj/src/main/Main.js
+++ kospj/src/main/Main.js
@@ -1,6 +1,6 @@
-import Card from 'react-bootstrap/Card';
-import CardGroup from 'react-bootstrap/CardGroup';
-import { Col, Row, Container } from 'react-bootstrap';
+import Card from "react-bootstrap/Card";
+import CardGroup from "react-bootstrap/CardGroup";
+import { Col, Row, Container } from "react-bootstrap";
 
 function Group({ apiList, menuOption }) {
   if (!!menuOption.length) {
@@ -10,7 +10,7 @@ function Group({ apiList, menuOption }) {
           {apiList.map((data) => {
             if (menuOption.includes(data.업태)) {
               return (
-                <Col key={data.연번} sm={4} menuoption={data['업태']}>
+                <Col key={data.연번} sm={4} menuoption={data["업태"]}>
                   <CardGroup className="container fluid">
                     <Card>
                       <Card.Img
@@ -19,11 +19,11 @@ function Group({ apiList, menuOption }) {
                         // "https://w.namu.la/s/aa411ec55b7d4906fb6107bc6966e312fd6d884d5704c36a2dd46f2d183a42b52589730964984792cca317b20bd45440ad4e5fd531fd31ecc127101bd54647bcea01407e262a997892594d72862f70793e9a117e45b03ea71e093fce5bf33f4039c0f5ee8eb6d476aa53cb4f533046c1"
                       />
                       <Card.Body>
-                        <Card.Title>{data['업 소 명']}</Card.Title>
-                        <Card.Text>{data['소재지']}</Card.Text>
+                        <Card.Title>{data["업 소 명"]}</Card.Title>
+                        <Card.Text>{data["소재지"]}</Card.Text>
                       </Card.Body>
                       <Card.Footer>
-                        <small className="text-muted">{data['전화번호']}</small>
+                        <small className="text-muted">{data["전화번호"]}</small>
                       </Card.Footer>
                     </Card>
                   </CardGroup>
@@ -40,7 +40,7 @@ function Group({ apiList, menuOption }) {
         <Row>
           {apiList.map((data) => {
             return (
-              <Col key={data['연번']} sm={4} menuoption={data['업태']}>
+              <Col key={data["연번"]} sm={4} menuoption={data["업태"]}>
                 <CardGroup className="container fluid">
                   <Card>
                     <Card.Img
@@ -49,11 +49,11 @@ function Group({ apiList, menuOption }) {
                       // "https://w.namu.la/s/aa411ec55b7d4906fb6107bc6966e312fd6d884d5704c36a2dd46f2d183a42b52589730964984792cca317b20bd45440ad4e5fd531fd31ecc127101bd54647bcea01407e262a997892594d72862f70793e9a117e45b03ea71e093fce5bf33f4039c0f5ee8eb6d476aa53cb4f533046c1"
                     />
                     <Card.Body>
-                      <Card.Title>{data['업 소 명']}</Card.Title>
-                      <Card.Text>{data['소재지']}</Card.Text>
+                      <Card.Title>{data["업 소 명"]}</Card.Title>
+                      <Card.Text>{data["소재지"]}</Card.Text>
                     </Card.Body>
                     <Card.Footer>
-                      <small className="text-muted">{data['전화번호']}</small>
+                      <small className="text-muted">{data["전화번호"]}</small>
                     </Card.Footer>
                   </Card>
                 </CardGroup>
--- kospj/src/nav/Nav.js
+++ kospj/src/nav/Nav.js
@@ -6,28 +6,28 @@ import Navbar from "react-bootstrap/Navbar";
 import { MdOutlineRateReview, MdStar, MdFavorite, MdArrowUpward } from "react-icons/md";
 
 export default function ColorSchemesExample() {
-    return (
-        <>
-            <Navbar className="position-fixed top-0 end-0" bg="dark" variant="dark">
-                <Container className="d-flex flex-column vh-100 justify-content-center">
-                    {/* <Navbar.Brand href="#home"></Navbar.Brand> */}
-                    <Nav className="ml-auto d-flex flex-column">
-                        <Nav.Link href="#home">
-                            {" "}
-                            <MdOutlineRateReview style={{ fontSize: 30 }} />
-                        </Nav.Link>
-                        <Nav.Link href="#home">
-                            <MdStar style={{ fontSize: 30 }} />
-                        </Nav.Link>
-                        <Nav.Link href="#features">
-                            <MdFavorite style={{ fontSize: 30 }} />
-                        </Nav.Link>
-                        <Nav.Link href="#pricing">
-                            <MdArrowUpward style={{ fontSize: 30 }} />
-                        </Nav.Link>
-                    </Nav>
-                </Container>
-            </Navbar>
-        </>
-    );
+  return (
+    <>
+      <Navbar className="position-fixed top-0 end-0" bg="dark" variant="dark">
+        <Container className="d-flex flex-column vh-100 justify-content-center">
+          {/* <Navbar.Brand href="#home"></Navbar.Brand> */}
+          <Nav className="ml-auto d-flex flex-column">
+            <Nav.Link href="#home">
+              {" "}
+              <MdOutlineRateReview style={{ fontSize: 30 }} />
+            </Nav.Link>
+            <Nav.Link href="#home">
+              <MdStar style={{ fontSize: 30 }} />
+            </Nav.Link>
+            <Nav.Link href="#features">
+              <MdFavorite style={{ fontSize: 30 }} />
+            </Nav.Link>
+            <Nav.Link href="#root">
+              <MdArrowUpward style={{ fontSize: 30 }} />
+            </Nav.Link>
+          </Nav>
+        </Container>
+      </Navbar>
+    </>
+  );
 }
